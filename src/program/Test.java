b320bd9b090e29413849196ce6b13217613d0fec
PhKi <Philipp.Kirste@gmx.de>
2016-01-20 15:39:26 +0100

    added test classes

---
+++ src/program/Test.java
@@ -0,0 +1,12 @@
+package program;
+
+/**
+ * Created by Philipp on 20.01.2016.
+ */
+public class Test {
+    public Test(){}
+
+    public void write(){
+        System.out.println("Hallo");
+    }
+}
---
+++ src/unit_tests/TestTest.java
@@ -0,0 +1,13 @@
+package unit_tests;
+
+import program.Test;
+
+/**
+ * Created by Philipp on 20.01.2016.
+ */
+public class TestTest {
+    public static void main(String[] args){
+        Test test = new Test();
+        test.write();
+    }
+}
